a0bd8aa315fa8b8b174ee6ec98a61075bcd6d30a
kevin-weitgenant <kevin.weitgenant@gmail.com>
2022-01-30 16:15:17 -0300

    improved goodreads other editions scraper output

--- blinkist_GoodReads/spiders/GoodReads.py
+++ blinkist_GoodReads/spiders/GoodReads.py
@@ -1,8 +1,5 @@
 import scrapy
 
-#provavelmente um erro com os links relativos, request.follow
-
-
 class GoodreadsSpider(scrapy.Spider):
     name = 'GoodReads'
     allowed_domains = ['goodreads.com']
@@ -19,28 +16,26 @@ class GoodreadsSpider(scrapy.Spider):
             urls.append(titulo.xpath('.//td[@class= "field title"]/div/a/@href').get())
 
         for url in urls:
-            yield response.follow(url = url, callback = self.parse_check_otherEditions)
+            yield response.follow(url = url, callback = self.parse_check_otherEditions)   #get all possible names for the book
 
         if next_page:
-            yield response.follow(url=next_page, callback=self.parse)
+            yield response.follow(url=next_page, callback=self.parse) #pagination
 
     
-    def parse_check_otherEditions(self,response):   
+    def parse_check_otherEditions(self,response):   #check for other editions button
         
         other_editions = response.xpath('//div[@class = "coverButton" and contains(a,"Other editions")]/a/@href').get()
         
         if other_editions:
             yield response.follow(url=other_editions, callback=self.parse_otherEditions, meta = {"book_url": response.url})
             
-
-
-    def parse_otherEditions(self,response):
+    def parse_otherEditions(self,response): #function to get all possible names for the book(with pagination)
         
         other_editions = response.xpath('//a[@class="bookTitle"]/text()').getall()
         next_page = response.xpath("//a[@class = 'next_page']/@href").get()
         
-        if next_page:
-            yield response.follow(url=next_page, callback=self.parse_otherEditions, meta = {"book_url": response.url})
+        if next_page: #pagination
+            yield response.follow(url=next_page, callback=self.parse_otherEditions, meta = {"book_url":  response.request.meta["book_url"]})
 
         yield{
            response.request.meta["book_url"] : other_editions
